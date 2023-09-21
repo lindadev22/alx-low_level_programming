9de2d05fd00f34402c0f494de22ce2527f9a2d47
Linda <pwitylinda@gmail.com>
2023-09-21 08:35:56 -0700

    prints buffer function

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+README file for pointer arrays and strings
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,9 @@
+/**
+ * _putchar - writes the char c to stdout
+ * @c: Ther char to be printed
+ * Return: 1 on sucess
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
